ace89709c6044efa4853b0684a7f88e9de6311e7
Ayesha <ayeshaece111@gmail.com>
2026-03-09 11:34:09 +0530

    restored build.sh file

--- build.sh
+++ build.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
 IMAGE_NAME=ecommerce-app-react
-DEV_REPO=ayeshadockerhub/react-dev-repo
-PROD_REPO=ayeshadockerhub/react-prod-repo
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#build image
 
-#Building docker image
 echo "building docker image"
 
 docker build -t $IMAGE_NAME .
 
-echo "Build Completed...."
-
-
-if [ $BRANCH = 'dev' ]; then
-	docker tag $IMAGE_NAME $DEV_REPO:v1
-	docker push  $DEV_REPO:v1
-
-fi
-
-if [ $BRANCH = 'main' ]; then
-	docker tag $IMAGE_NAME $PROD_REPO:v1
-	docker push  $PROD_REPO:v1
-
-fi
-
-
-
-
-
-
-
+echo "Build completed ....."
 
