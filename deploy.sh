d52babba06bc0b5312671c6d8f9238c9cb076c77
ayesha803 <81418736+ayesha803@users.noreply.github.com>
2026-03-09 17:05:47 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 SERVER=35.170.75.177
+REPO=https://github.com/sriram-R-krishnan/devops-build.git
+DIR=devops-build
 
 ssh -o StrictHostKeyChecking=no ubuntu@$SERVER << EOF
 
-cd devops-build
+if [ ! -d "$DIR" ]; then
+    git clone $REPO
+fi
+
+cd $DIR
 git pull
 
 docker-compose pull
